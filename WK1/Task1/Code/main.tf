a3e313c174c0c985394385bafab24af1183015bc
Fikayo Oluwakeye <fikayofakeye@yahoo.com>
2024-06-05 22:37:03 -0600

    GITIGNORE AND iac Code

---
+++ WK1/Task1/Code/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    profile = "fikayo"
+    region = "us-east-1"
+    key = "terraform.tfstate"
+    bucket = "my-example-s3-bucket-2024"
+  }
+}
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "Vpc" {
+  source = "./modules/vpc"
+}
---
+++ WK1/Task1/Code/modules/vpc/variables.tf
@@ -0,0 +1,9 @@
+variable "ENI_Name" {
+  default = "My-ENI"
+  type = string
+  
+}
+
+variable "ENI_IP" {
+  default = "10.0.0.16"
+}
