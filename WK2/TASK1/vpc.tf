bb6040e8c1ffb656da0bd0f413aa127131ab7c86
Fikayo Oluwakeye <fikayofakeye@yahoo.com>
2024-06-12 20:15:39 -0600

    fix vpc issue

--- WK2/TASK1/vpc.tf
+++ WK2/TASK1/vpc.tf
@@ -47,7 +47,7 @@ resource "aws_route_table_association" "Public_subnet_association" {
   route_table_id = aws_route_table.public_route.id  
 }
 
-resource "aws_route_table_association" "Public_subnet_association" {
+resource "aws_route_table_association" "Public_subnet_association1" {
   subnet_id = aws_subnet.public2.id
   route_table_id = aws_route_table.public_route.id  
 }
