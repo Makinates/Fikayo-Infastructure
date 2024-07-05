fa60de76bc23393266b1c999269620fe9acb220e
Fikayo Oluwakeye <fikayofakeye@yahoo.com>
2024-07-05 05:03:26 -0600

    Add secretmanagement role

--- WK4/TASK1/modules/vpc/SecretManager.tf
+++ WK4/TASK1/modules/vpc/SecretManager.tf
@@ -1,8 +1,8 @@
-resource "aws_secretsmanager_secret" "my_secret" {
-  name = "MySecret"  # Replace with your secret name
+resource "aws_secretsmanager_secret" "my_database_secret" {
+  name = "databaseCredentials"  # Replace with your secret name
 }
 
-resource "aws_secretsmanager_secret_version" "my_secret_version" {
+resource "aws_secretsmanager_secret_version" "my_database_secret_version" {
   secret_id     = aws_secretsmanager_secret.my_secret.id
   secret_string = jsonencode({
     username = "my_username",
@@ -11,6 +11,20 @@ resource "aws_secretsmanager_secret_version" "my_secret_version" {
   })
 }
 
-output "secret_value" {
-  value = aws_secretsmanager_secret_version.my_secret_version.secret_string
+resource "aws_secretsmanager_secret" "my_Api_secret" {
+  name = "apiCredentials"  # Replace with your secret name
 }
+
+resource "aws_secretsmanager_secret_version" "my_Api_secret_version" {
+  secret_id     = aws_secretsmanager_secret.my_Api_secret.id
+  secret_string = jsonencode({
+    username = "my_username11",
+    password = "my_password11"
+    # Add more key-value pairs as needed
+  })
+}
+
+
+# output "secret_value" {
+#   value = aws_secretsmanager_secret_version.my_secret_version.secret_string
+# }
--- WK4/TASK1/modules/vpc/roles.tf
+++ WK4/TASK1/modules/vpc/roles.tf
@@ -101,3 +101,50 @@ resource "aws_iam_role" "operations" {
     ]
   })
 }
+
+resource "aws_iam_role_policy_attachment" "attach_policy" {
+  role       = aws_iam_role.operations.name
+  policy_arn = aws_iam_policy.vpc_policy.arn
+}
+
+
+resource "aws_iam_role" "secrets_management_role" {
+  name               = "Developers"
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "ec2.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_policy" "secrets_management_policy" {
+  name = "secretsManagementPolicy"
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": "secretsmanager:GetSecretValue",
+        "Resource": [
+          aws_secretsmanager_secret.my_database_secret.arn,
+          aws_secretsmanager_secret.my_Api_secret.arn
+        ]
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "attach_policy" {
+  role       = aws_iam_role.secrets_management_role.name
+  policy_arn = aws_iam_policy.secrets_management_policy.arn
+}
